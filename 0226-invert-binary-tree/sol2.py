1e15f9ecc1226833f02c2562d66b4eacd21bdab7
Praneetha Parupudi <142181893+praneetha0812@users.noreply.github.com>
2024-09-13 12:38:54 -0400

    Add files via upload

---
+++ 0226-invert-binary-tree/sol2.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        def reverseNodes(child):
+            if child is None:
+                return
+            # Recursively invert left and right subtrees
+            reverseNodes(child.left)
+            reverseNodes(child.right)
+            
+            # Swap the left and right children
+            hold = child.left
+            child.left = child.right
+            child.right = hold
+        
+        # Start the recursive inversion from the root
+        reverseNodes(root)
+        return root
